99e3a8f05a052431ef812a291df6e69e54b634df
Scott Kidder <kidder.scott@gmail.com>
2014-05-13 21:36:07 -0700

    Cleaned up code formatting

--- door_alarm.py
+++ door_alarm.py
@@ -1,25 +1,32 @@
 import Adafruit_BBIO.ADC as ADC
 import time
 
-sensor_pin = 'P9_40'
-is_open=True
-close_counter=0
+SENSOR_PIN = 'P9_40'
+DOOR_CLOSED_DETECTION_PERIOD_SECS = 2
+SENSOR_SAMPLE_FREQUENCY = 0.1
+DOOR_CLOSED_SAMPLES = DOOR_CLOSED_DETECTION_PERIOD_SECS * (1/SENSOR_SAMPLE_FREQUENCY)
+SENSOR_THRESHOLD = 0.1
 
-ADC.setup()
+def monitor_sensor():
+    """Read the sensor value and detect an alarm condition."""
+    ADC.setup()
 
-while True:
-    reading = ADC.read(sensor_pin)
-    if is_open == False and reading > 0.1:
-        print "Door opened"
-        is_open=True
-    elif is_open == True:
-        if reading < 0.1:
-            close_counter += 1
-            if close_counter >= 30:  # door sensor must read low for 3 seconds
-                is_open=False
-                print "Door closed"
-                close_counter = 0
-        else:
-            close_counter=0          # reset the close counter, false alarm
-    time.sleep(0.1)
+    is_open = True
+    closed_samples_counter = 0
+    while True:
+        reading = ADC.read(SENSOR_PIN)
+        if is_open == False and reading > SENSOR_THRESHOLD:
+            print "Door opened"
+            is_open = True
+        elif is_open == True:
+            if reading < SENSOR_THRESHOLD:
+                closed_samples_counter += 1
+                if closed_samples_counter >= DOOR_CLOSED_SAMPLES:
+                    is_open = False
+                    print "Door closed"
+                    closed_samples_counter = 0
+            else:
+                closed_samples_counter = 0
+        time.sleep(SENSOR_SAMPLE_FREQUENCY)
 
+monitor_sensor()
