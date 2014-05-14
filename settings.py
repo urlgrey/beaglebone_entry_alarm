5acc1ff2df2ecd374b785006ecbfed9d78a47454
Scott Kidder <kidder.scott@gmail.com>
2014-05-13 22:00:20 -0700

    Start Twitter integration

---
+++ settings.py
@@ -0,0 +1,6 @@
+"""Settings for the door alarm project"""
+
+CONSUMER_KEY = 'consumer_key'
+CONSUMER_SECRET = 'consumer_secret'
+ACCESS_TOKEN_KEY = 'access_token_key'
+ACCESS_TOKEN_SECRET = 'access_token_secret'
