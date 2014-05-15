2f3b1704176dbd5f668db2722cddc679940a412a
Scott Kidder <kidder.scott@gmail.com>
2014-05-14 18:19:22 -0700

    Send direct message to Twitter

--- door_alarm.py
+++ door_alarm.py
@@ -10,16 +10,18 @@ DOOR_CLOSED_SAMPLES = DOOR_CLOSED_DETECTION_PERIOD_SECS * (1/SAMPLE_FREQUENCY)
 SENSOR_THRESHOLD = 0.1
 
 def post_to_twitter(msg):
+    """Send a personal message to Twitter"""
     api = twitter.Api(consumer_key=settings.CONSUMER_KEY,
                       consumer_secret=settings.CONSUMER_SECRET,
                       access_token_key=settings.ACCESS_TOKEN_KEY,
                       access_token_secret=settings.ACCESS_TOKEN_SECRET)
-    print api.VerifyCredentials()
+    api.PostDirectMessage(text=msg,
+                          screen_name=settings.TWITTER_SCREEN_NAME)
 
 def monitor_sensor():
     """Read the sensor value and detect an alarm condition."""
 
-    post_to_twitter("hello, world!")
+    post_to_twitter("Door Entry Alarm: started")
     ADC.setup()
 
     is_open = True
@@ -28,6 +30,7 @@ def monitor_sensor():
         reading = ADC.read(SENSOR_PIN)
         if is_open == False and reading > SENSOR_THRESHOLD:
             print "Door opened"
+            post_to_twitter("Door opened")
             is_open = True
         elif is_open == True:
             if reading < SENSOR_THRESHOLD:
@@ -35,6 +38,7 @@ def monitor_sensor():
                 if closed_samples_counter >= DOOR_CLOSED_SAMPLES:
                     is_open = False
                     print "Door closed"
+                    post_to_twitter("Door closed")
                     closed_samples_counter = 0
             else:
                 closed_samples_counter = 0
